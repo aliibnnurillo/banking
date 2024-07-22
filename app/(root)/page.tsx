4c141ec1b819cc14610028a4f6cd5790a10f6cef
Nurillo_96 <kimsanboyevnurillo@gmail.com>
2024-07-22 18:17:04 +0500

    try to add some page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
+  const loggedIn = {firstName: "Nurillo"}
   return (
     <section className="home">
       <div className="home-content">
-        <header className="home-header"> Welcome !</header>
+        <header className="home-header">
+          <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName || 'Gohst'} subtext="Accsess and manage your account transactions efficiently."/>
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35}/>
+        </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar banks={[]} transactions={[]} user={loggedIn}/>
     </section>
   );
 };
